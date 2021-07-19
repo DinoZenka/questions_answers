47be00202c2dac9850dda3d1e45d89c7aa19fdac
DinoZenka <maxim.puchko123@gmail.com>
2021-07-19 15:28:47 +0300

    Added fields validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import React from 'react';
 import Question from './components/question';
+import Swal from 'sweetalert2';
+import Confetti from 'react-confetti';
 
-function App() {
+const App = () => {
+
   const [questions, setQuestions] = React.useState([]);
   const [answers, setAnswers] = React.useState({});
-  const [isLoading, setLoading] = React.useState(true);
+  const [isLoading, setLoading] = React.useState(false);
+  const [isAllValid, setAllValid] = React.useState(true);
+  const [isSuccess, setSuccess] = React.useState(false);
+
+  let allValids = React.useRef({});
 
   React.useEffect(() => {
     const getchData = async () => {
       setLoading(false);
-      const data = await fetch('data.json').then(data => data.json());
+      const data = await fetch('questions_answers/data.json').then(data => data.json());
       setQuestions(data.data);
       setLoading(true);
     }
@@ -18,41 +25,60 @@ function App() {
   }, []);
 
   React.useEffect(() => {
+
     const newObj = {};
     for (let i of questions) {
       const val = localStorage.getItem(i.question);
       newObj[i.question] = val ? val : "";
     }
     setAnswers(newObj);
-  }, [questions])
+  }, [questions]);
 
-  const onChange = (event) => {
-    const newAnswers = {
-      ...answers,
-      [event.target.name]: event.target.value
-    }
-    setAnswers(newAnswers);
+  const isFormValid = () => {
+    return allValids;
   }
 
+  const ques = questions.map(elem => {
+    return <Question
+      question={elem.question}
+      key={elem.id}
+      id={elem.id}
+      isFormValid={isFormValid}
+      initial={answers[elem.question]}
+    />
+  });
+
   const onSend = (e) => {
-    const keys = Object.keys(answers);
-    let flag = false;
-    const newObj = {}
-    for (let i of keys) {
-      if (answers[i] === "") {
-        flag = true;
-      }
-      localStorage.setItem(i, answers[i]);
-      newObj[i] = "";
+    let canSend = true;
+    for (const input of Object.values(allValids.current)) {
+      canSend &= input.b;
     }
-    if (flag) {
-      localStorage.clear();
-      alert('Введите все поля');
-    } else {
-      alert(`Данные сохранены!`);
-      setAnswers(newObj);
+    setAllValid(canSend);
+    const newObj = { ...answers };
+
+    if (canSend) {
+      Object.values(allValids.current).forEach(el => {
+        const [quest, answ] = Object.entries(el)[1];
+        localStorage.setItem(quest, answ);
+        newObj[quest] = "";
+      });
+      setSuccess(true);
+      setTimeout(() => {
+        Swal.fire({
+          title: "Сохранение даных", text: "Данные успешно сохранены в localStorage", icon: "success", didClose: () => {
+            setSuccess(false);
+          }
+        });
+      }, 200);
+
     }
+
+    setAnswers(newObj);
     e.preventDefault();
+  }
+
+  if (isSuccess) {
+    return <Confetti />
   }
 
   return (
@@ -61,23 +87,14 @@ function App() {
         isLoading
           ? <form className="form">
             {
-              questions.map(elem => {
-                const newValue = answers[elem.question];
-                return <Question
-                  question={elem.question}
-                  key={elem.id}
-                  id={elem.id}
-                  changeVal={onChange}
-                  value={newValue ? newValue : ""}
-                />
-              })
+              ques
             }
+            {(!isAllValid) && <div style={{ color: "red" }}>Введите все поля</div>}
             <button className="send-button" onClick={onSend}>Отправить</button>
           </form>
           : <h1>Loading data...</h1>
       }
     </div>
-
   );
 }
 
--- src/components/question.js
+++ src/components/question.js
@@ -1,19 +1,93 @@
 import React from 'react';
 
-const Question = ({ question, id, changeVal, value }) => {
+
+const useValidation = (value, validators) => {
+  const [isEmpty, setEmpty] = React.useState(true);
+  const [isMinLength, setMinLength] = React.useState(true);
+  const [isInputValid, setInputValid] = React.useState(true);
+
+
+  React.useEffect(() => {
+    for (const valid in validators) {
+      switch (valid) {
+        case "isEmpty": {
+          value ? setEmpty(false) : setEmpty(true);
+          break;
+        }
+        case "minLength": {
+          value.length >= validators[valid] ? setMinLength(false) : setMinLength(true);
+          break;
+        }
+      }
+    }
+  }, [value]);
+
+  React.useEffect(() => {
+    if (isEmpty || isMinLength) {
+      setInputValid(false);
+    } else {
+      setInputValid(true);
+    }
+  }, [isMinLength, isEmpty]);
+
+
+  return {
+    isEmpty,
+    isMinLength,
+    isInputValid
+  }
+}
+
+
+
+const useInput = (initialValue, validations, isReset) => {
+  const [value, setValue] = React.useState(initialValue);
+  const [isDirty, setDirty] = React.useState(false);
+  const valid = useValidation(value, validations);
+
+  if (value !== "" && initialValue === "") {
+    setValue("");
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const onBlur = (e) => {
+    setDirty(true);
+  }
+
+  return {
+    value,
+    onChange,
+    onBlur,
+    isDirty,
+    ...valid
+  }
+}
+
+
+const Question = ({ question, id, isFormValid, initial }) => {
+  const val = useInput(initial, { isEmpty: true, minLength: 2 }, initial === "");
+
+  isFormValid().current[id] = { b: val.isInputValid, [question]: val.value };
 
   return (
     <div className="question-container">
       <label className="question" htmlFor={`input${id}`}>{question}</label>
+
+      {(val.isDirty && val.isEmpty) && <div style={{ color: 'red' }}>Поле не может быть пустым</div>}
+      {(val.isDirty && val.isMinLength) && <div style={{ color: 'red' }}>Минимальная длина текста 2 символа</div>}
       <input
         id={`input${id}`}
         className="answer"
         type="text"
-        value={value}
-        onChange={changeVal}
+        value={val.value}
+        onChange={e => val.onChange(e)}
+        onBlur={e => val.onBlur(e)}
         name={question}
         placeholder={"*Введите текст"}
-        required={true}
+        required
       ></input>
     </div>
   )
